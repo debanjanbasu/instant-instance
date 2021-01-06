7c85bbbcb52f748963884751036801baf7d81300
Debanjan Basu <debanjanbasu2006@gmail.com>
2021-01-06 20:14:11 +1100

    updated bakend for encryption

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@
 # be bootstrapped according to the simple yet essential procedure in
 # https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
 module "terraform_state_backend" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=master"
-  namespace  = "instant-instance"
-  stage      = "build"
-  name       = "terraform-state-bucket"
-  attributes = ["state"]
+  source                        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=master"
+  namespace                     = "instant-instance"
+  stage                         = "build"
+  name                          = "terraform-state-bucket"
+  attributes                    = ["state"]
+  enable_server_side_encryption = true
+  enable_public_access_block    = true
 
   terraform_backend_config_file_path = "."
   terraform_backend_config_file_name = "backend.tf"
